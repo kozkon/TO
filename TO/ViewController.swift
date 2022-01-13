c83a1bda21d04a87e2d4be68caede873a74d8a2e
konstantin.a.kozlov <konstantin.a.kozlov@yandex.ru>
2022-01-13 18:14:59 +0300

    Initial Commit

---
+++ TO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TO
+//
+//  Created by Константин Козлов on 13.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
